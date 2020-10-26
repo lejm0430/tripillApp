ea8cd25702f91697b287994bafd5571ae7a35a26
Y27055 <64213650+Y27055@users.noreply.github.com>
2020-10-26 18:14:04 +0900

    clear

--- app/src/main/java/com/example/tripill/MainActivity.java
+++ app/src/main/java/com/example/tripill/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.tripill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "푸쉬 테스트_yj", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(),"asdf",Toast.LENGTH_SHORT);
+
+
     }
 }

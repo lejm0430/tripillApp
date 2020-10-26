919c7b2219cedc1c3fec73c7f091f6e546313e32
lejm0430 <lejm0430@naver.com>
2020-10-26 17:58:59 +0900

    qwer

--- app/src/main/java/com/example/tripill/MainActivity.java
+++ app/src/main/java/com/example/tripill/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(getApplicationContext(),"asdf",Toast.LENGTH_SHORT);
     }
 }

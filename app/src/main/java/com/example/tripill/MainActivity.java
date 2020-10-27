26fdee64ecf43507d3864d96ad6701709cc01596
Y27055 <64213650+Y27055@users.noreply.github.com>
2020-10-28 01:21:24 +0900

    YJ

--- app/src/main/java/com/example/tripill/MainActivity.java
+++ app/src/main/java/com/example/tripill/MainActivity.java
@@ -2,14 +2,81 @@ package com.example.tripill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_age);
+        setContentView(R.layout.activity_main);
+
+        ImageView menuBtn = findViewById(R.id.menuBtn);
+        final ImageView headBtn = findViewById(R.id.headBtn_ring);
+        ImageView neckBtn = findViewById(R.id.neckBtn_ring);
+        ImageView stomachBtn = findViewById(R.id.stomachBtn_ring);
+        ImageView armBtn = findViewById(R.id.armBtn_ring);
+        ImageView legBtn = findViewById(R.id.legBtn_ring);
+        ImageView musclePainBtn = findViewById(R.id.musclePainBtn_ring);
+        ImageView burnBtn = findViewById(R.id.burnBtn_ring);
+        ImageView woundBtn = findViewById(R.id.woundBtn_ring);
+        ImageView beerBtn = findViewById(R.id.beerBtn_ring);
+
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        headBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        neckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        stomachBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        armBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        legBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        musclePainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        burnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        woundBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
+
+        beerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {           }
+        });
 
     }
 }

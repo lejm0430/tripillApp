f54b585441a4531e662b52795514f0faf66ad2ac
lejm0430 <lejm0430@naver.com>
2020-10-27 17:49:25 +0900

    asdf

--- app/src/main/java/com/example/tripill/MainActivity.java
+++ app/src/main/java/com/example/tripill/MainActivity.java
@@ -13,6 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),"asdf",Toast.LENGTH_SHORT);
     }
 }

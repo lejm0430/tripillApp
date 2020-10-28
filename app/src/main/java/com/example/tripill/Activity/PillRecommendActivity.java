5645fe0afa00e8a4647daacf25f2729b02ae8f20
lejm0430 <lejm0430@naver.com>
2020-10-28 18:32:32 +0900

    sos

---
+++ app/src/main/java/com/example/tripill/Activity/PillRecommendActivity.java
@@ -0,0 +1,16 @@
+package com.example.tripill.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripill.R;
+
+public class PillRecommendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_recommend);
+    }
+}

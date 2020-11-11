6b9c51988f4bc7361b6789be83449c94d8012921
Y27055 <64213650+Y27055@users.noreply.github.com>
2020-11-11 15:00:14 +0900

    test1

--- app/src/main/java/com/example/tripill/Activity/PillRecommendActivity.java
+++ app/src/main/java/com/example/tripill/Activity/PillRecommendActivity.java
@@ -391,8 +391,8 @@ public class PillRecommendActivity extends AppCompatActivity implements TextToSp
         final RelativeLayout layout=(RelativeLayout) dlg.findViewById(R.id.layout);
         final TextView text=(TextView) dlg.findViewById(R.id.text);
 
-        text.setText("당신의 증상과 위치가\n응급메시지로 전송됩니다.\n정말 메시지를 전송할까요?");
-        ok.setText("전송");
+        text.setText(R.string.sos);
+        ok.setText(R.string.send);
 
         layout.setClipToOutline(true);
 
